a54376fa6ea35179cb62754b37c4b39ba31753fc
eriksonlizandro <eegs.sist@gmail.com>
2020-04-05 21:50:03 -0700

    test

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends Component {
+    constructor() {
+
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    // With anything that comes from React, so constructor and render pre-build in React
+    // Any time you make your own methods on a component, use this syntax  'onSearchChange = (event) => {' 
+    // so arrow functions, and this makes that the "this" value is according to where it was created
+    // which in this case was the "App"
+    // Use " this.setState" every time you want to change "state" 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })  
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase);
+        })
+
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+}
+
+export default App; 
---
+++ src/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './card';
+
+//Here we created a function 
+//This function creates a loop using .map 
+//And it gets the information from the file "robots"
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+
+                    return (
+                        <Card key={i} //This key is used to identfy the cards 
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            username={robots[i].username}
+                            email={robots[i].email}
+                        />
+                    );
+                })
+
+            }
+        </div>
+    );
+}
+export default CardList; 
