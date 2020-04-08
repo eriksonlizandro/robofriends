d08dc2876244d7be304fea34d374e938ab8a65a0
eriksonlizandro <eegs.sist@gmail.com>
2020-04-07 20:59:04 -0700

    New title

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import './App.css'; 
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots:[],
+      robots: [],
       searchfield: '',
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({robots: users}));
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   // With anything that comes from React, so constructor and render pre-build in React
@@ -35,19 +36,20 @@ class App extends Component {
         .includes(this.state.searchfield.toLocaleLowerCase())
     );
 
-     return !this.state.robots.length ? <h1 className='dt center pv7-ns'>Loading...</h1>: 
+    return !this.state.robots.length ? <h1 className='dt center pv7-ns'>  Loading...</h1> : //ternary operator 
 
-   (<div className='tc'>
+      (<div className='tc'>
         <h1 className='f1'>Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-        <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
-    );
-    
+      );
+
   }
 }
 
 export default App;
- 
